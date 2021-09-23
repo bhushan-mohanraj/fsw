ef9eb60bb5572cb2ba5b76b238047e65568cf2f2
Bhushan <50306448+bhushanmohanraj@users.noreply.github.com>
2021-09-23 13:18:08 -0400

    Format `setup.py`

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     description="Tools and guides for building apps with Flask, SQLAlchemy, and WTForms.",
     install_requires=[
         "Flask>=2",
-        "SQLAlchemy>=1.4,<2"
+        "SQLAlchemy>=1.4,<2",
         "WTForms>=2.3,<3",
-    ]
+    ],
 )
