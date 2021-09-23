602a6299659f941e13c8d9ecfb6db9fefdb90b03
Bhushan <50306448+bhushanmohanraj@users.noreply.github.com>
2021-09-23 12:55:30 -0400

    Add `setup.py` and `requirements.txt`

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="fsw",
+    version="0.0.0",
+    author="Bhushan Mohanraj",
+    description="Tools and guides for building apps with Flask, SQLAlchemy, and WTForms.",
+    install_requires=[
+        "Flask>=2",
+        "SQLAlchemy>=1.4,<2"
+        "WTForms>=2.3,<3",
+    ]
+)
