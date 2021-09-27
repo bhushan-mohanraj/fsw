dd0286c2f9277020d87fe915b9e4a48f76bacfa5
Bhushan <50306448+bhushanmohanraj@users.noreply.github.com>
2021-09-27 07:30:50 -0400

    Update Flask version requirement

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     author="Bhushan Mohanraj",
     description="Tools and guides for building apps with Flask, SQLAlchemy, and WTForms.",
     install_requires=[
-        "Flask>=2",
+        "Flask>=2.0",
         "SQLAlchemy>=1.4,<2",
         "WTForms>=2.3,<3",
     ],
