6b071b99689970378fb8383fba8c23ab9de727f2
Bhushan <50306448+bhushanmohanraj@users.noreply.github.com>
2021-10-06 09:16:55 -0400

    Restructure model mixins

--- fsw/models/__init__.py
+++ fsw/models/__init__.py
@@ -1,10 +1,8 @@
-from fsw.models.mixins import (
-    IDMixin,
-    NameMixin,
-)
+from fsw.models.mixins.id import IDMixin
+from fsw.models.mixins.name import ClassNameMixin
 
 
 __all__ = [
     "IDMixin",
-    "NameMixin",
+    "ClassNameMixin",
 ]
