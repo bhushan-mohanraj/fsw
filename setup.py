0c66b696ec1cb0a1f6e753ba0d86e591deccc725
Bhushan <50306448+bhushanmohanraj@users.noreply.github.com>
2021-10-19 15:36:24 -0400

    Update `setup.py`

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ import setuptools
 setuptools.setup(
     name="fsw",
     version="0.1.1",
-    author="Bhushan Mohanraj",
     description="Tools and guides for building apps with Flask, SQLAlchemy, and WTForms.",
+    url="https://github.com/bhushanmohanraj/fsw",
+    author="Bhushan Mohanraj",
+    packages=setuptools.find_packages(),
     install_requires=[
         "Flask>=2.0",
         "SQLAlchemy>=1.4,<2",
         "WTForms>=2.3,<3",
     ],
+    python_requires='>=3.6',
 )
