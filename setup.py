e272757ee9af3400318718ec52403f22fc90b91f
Bhushan <50306448+bhushanmohanraj@users.noreply.github.com>
2021-10-19 15:17:52 -0400

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="fsw",
-    version="0.1.0",
+    version="0.1.1",
     author="Bhushan Mohanraj",
     description="Tools and guides for building apps with Flask, SQLAlchemy, and WTForms.",
     install_requires=[
