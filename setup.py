a8fa9c8c6adf4a739794e229c92ef44d0bb7c421
Bhushan <50306448+bhushanmohanraj@users.noreply.github.com>
2021-10-22 13:06:15 -0400

    Add long description to package setup

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 import setuptools
+import pathlib
+
+
+readme_path = pathlib.Path(__file__).parent.resolve() / "README.md"
 
 setuptools.setup(
     name="fsw",
     version="0.1.1",
     description="Tools and guides for building apps with Flask, SQLAlchemy, and WTForms.",
+    long_description=readme_path.read_text("utf-8"),
+    long_description_content_type="text/markdown",
     url="https://github.com/bhushanmohanraj/fsw",
     author="Bhushan Mohanraj",
     packages=setuptools.find_packages(),
