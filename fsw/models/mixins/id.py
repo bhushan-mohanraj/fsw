de17ea8d9ca64c8870aa0dc20160985cbf2dda42
Bhushan Mohanraj <50306448+bhushanmohanraj@users.noreply.github.com>
2021-11-23 09:33:43 -0500

    Add back `IDMixin` and `ClassNameMixin`

---
+++ fsw/models/mixins/id.py
@@ -0,0 +1,15 @@
+from sqlalchemy import (
+    Column,
+    types,
+)
+
+
+class IDMixin:
+    """
+    Add an integer, primary key ID column to a model.
+    """
+
+    id = Column(
+        types.Integer,
+        primary_key=True,
+    )
---
+++ fsw/models/mixins/name.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import (
+    declared_attr,
+)
+
+
+class ClassNameMixin:
+    """
+    Set the table name to the model class name.
+    """
+
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__
