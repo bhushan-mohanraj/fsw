27be37a2eb5cd537b23cad0cc8dfb88a3f34c02f
Bhushan Mohanraj <50306448+bhushanmohanraj@users.noreply.github.com>
2021-11-24 20:30:34 -0500

    Add `_fill` helper function

---
+++ fsw/helpers.py
@@ -0,0 +1,21 @@
+"""
+Helper functions used throughout the package.
+"""
+
+import warnings
+
+
+def _fill(instance, **kwargs) -> None:
+    """
+    Fill the attributes of an object instance with the given keyword arguments.
+
+    Raise a warning if a given keyword is not an attribute.
+    """
+
+    for key, value in kwargs.items():
+        if hasattr(instance, key):
+            setattr(instance, key, value)
+        else:
+            warnings.warn(
+                f"An instance of '{type(instance).__name__}' has no attribute '{key}'"
+            )
