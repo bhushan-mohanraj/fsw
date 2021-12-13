2642629d2aa7865b3e84e0fb8dc1b267bbb1059b
Bhushan Mohanraj <50306448+bhushanmohanraj@users.noreply.github.com>
2021-12-13 13:09:20 -0500

    Create template view

---
+++ fsw/views/template.py
@@ -0,0 +1,31 @@
+"""
+A view to render a template with context.
+"""
+
+import flask
+from flask import views
+
+
+class TemplateView(views.View):
+    """
+    A view to render a template with context.
+    """
+
+    template_name: str
+
+    def get_template_context(self) -> dict:
+        """
+        Get the context dictionary to render the template.
+        """
+
+        return {}
+
+    def dispatch_request(self, **kwargs):
+        """
+        Render the template with the given template context.
+        """
+
+        return flask.render_template(
+            self.template_name,
+            **self.get_template_context(),
+        )
