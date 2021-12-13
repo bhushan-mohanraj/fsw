71dde269819019eb7536888bb7eb1022c19ef3c1
Bhushan Mohanraj <50306448+bhushanmohanraj@users.noreply.github.com>
2021-12-13 13:08:49 -0500

    Create redirect view

---
+++ fsw/views/redirect.py
@@ -0,0 +1,21 @@
+"""
+A view to redirect to another URL.
+"""
+
+import flask
+from flask import views
+
+
+class RedirectView(views.View):
+    """
+    A view to redirect to another URL.
+    """
+
+    redirect_url: str  # The URL to redirect to.
+
+    def dispatch_request(self):
+        """
+        Redirect to the given URL.
+        """
+        
+        return flask.redirect(self.redirect_url)
