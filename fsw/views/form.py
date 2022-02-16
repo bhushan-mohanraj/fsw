0761fdbd892179b0a76c6cea2b3a7fb27a535b17
Bhushan Mohanraj <50306448+bhushanmohanraj@users.noreply.github.com>
2022-02-16 11:04:36 -0500

    Add placeholder methods for `FormView`.

--- fsw/views/form.py
+++ fsw/views/form.py
@@ -12,3 +12,37 @@ class FormView(TemplateView, RedirectView, flask.views.View):
     """
     A view class to render and process forms.
     """
+
+    # The form class.
+    form: type
+
+    # The current form instance.
+    form_instance = None
+
+    def _get_template_context(self):
+        """
+        Add the form instance to the template context.
+        """
+
+        # Get the template context provided by developers.
+        template_context = self.get_template_context()
+
+        # Add the current form instance to the context.
+        template_context["form_instance"] = self.form_instance
+
+        return template_context
+
+    def dispatch_valid_form_request(self):
+        pass
+
+    def _dispatch_valid_form_request(self):
+        pass
+
+    def dispatch_invalid_form_request(self):
+        pass
+
+    def _dispatch_invalid_form_request(self):
+        pass
+
+    def dispatch_request(self, **kwargs):
+        pass
