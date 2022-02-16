22c33eade80df9d0cdb306d6702ba037120b00d4
Bhushan Mohanraj <50306448+bhushanmohanraj@users.noreply.github.com>
2022-02-16 12:07:50 -0500

    Implement `dispatch_request` for `FormView`.

--- fsw/views/form.py
+++ fsw/views/form.py
@@ -2,6 +2,7 @@
 A view class to render and process forms.
 """
 
+import flask
 import flask.views
 
 from fsw.views.redirect import RedirectView
@@ -59,3 +60,17 @@ class FormView(TemplateView, RedirectView, flask.views.View):
         Render the form template for a GET request,
         and process the form data for a POST request.
         """
+
+        self.form_instance = self.form(flask.request.form)
+
+        # Process a request with submitted form data.
+        if flask.request.method == "POST":
+            # Dispatch a request with valid form data.
+            if self.form_instance.validate():
+                return self._dispatch_valid_form_request()
+
+            # Dispatch a request with invalid form data.
+            return self._dispatch_invalid_form_request()
+
+        # Render the template with the form for GET requests.
+        return TemplateView.dispatch_request(self)
