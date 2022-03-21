e9f34df2eee0641a1b477a852eed64795bc8d49e
Bhushan Mohanraj <50306448+bhushanmohanraj@users.noreply.github.com>
2022-03-21 16:37:34 -0400

    Check for falsy URL values rather than only `None`.

--- fsw/views/redirect.py
+++ fsw/views/redirect.py
@@ -44,7 +44,7 @@ class RedirectView(flask.views.View, RedirectMixin):
 
         redirect_url = self._get_redirect_url()
 
-        if redirect_url is None:
+        if not redirect_url:
             return flask.abort(404)
 
         return flask.redirect(redirect_url)
