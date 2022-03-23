b7b8e0af5fe1c12027eb94d99614c0be0fd5d8cb
Bhushan Mohanraj <50306448+bhushanmohanraj@users.noreply.github.com>
2022-03-22 20:51:56 -0400

    Import the classes for views, models, and forms.

---
+++ fsw/models/__init__.py
@@ -0,0 +1,14 @@
+from fsw.models.id import (
+    IDModelMixin,
+)
+
+from fsw.models.name import (
+    ClassNameModelMixin,
+)
+
+from fsw.models.crud import (
+    CreateModelMixin,
+    ReadModelMixin,
+    UpdateModelMixin,
+    DeleteModelMixin,
+)
