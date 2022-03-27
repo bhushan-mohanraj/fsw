2845ab96d6576208f0534bb607824507fb6e3296
Bhushan Mohanraj <50306448+bhushanmohanraj@users.noreply.github.com>
2022-03-27 13:25:25 -0400

    Add mixins for model views.

---
+++ fsw/views/crud.py
@@ -0,0 +1,46 @@
+"""
+Views to create, read, update, and delete model instances.
+"""
+
+class ModelViewMixin:
+    """
+    A mixin for views that process an SQLAlchemy model class.
+    """
+
+    # The SQLAlchemy database session.
+    database_session: None
+
+    # The model class.
+    model: type
+
+
+class ModelInstanceViewMixin(ModelViewMixin):
+    """
+    A mixin for views that process one model instance.
+    """
+
+    # The current model instance (during request dispatching).
+    model_instance = None
+
+    def get_model_instance(self):
+        """
+        Get the model instance.
+        """
+
+        return None
+
+
+class ModelInstancesViewMixin(ModelViewMixin):
+    """
+    A mixin for views that process multiple model instances.
+    """
+
+    # The current model instances (during request dispatching).
+    model_instances = []
+
+    def get_model_instances(self) -> list:
+        """
+        Get the model instances.
+        """
+
+        return []
