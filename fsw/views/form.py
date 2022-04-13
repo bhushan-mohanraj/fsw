2784e4c6219df45e581c5f0a526a9e4b71a43259
Bhushan Mohanraj <50306448+bhushanmohanraj@users.noreply.github.com>
2022-04-13 10:02:21 -0400

    Add a default value for `TemplateViewMixin._get_template_context()`.

--- fsw/views/form.py
+++ fsw/views/form.py
@@ -25,7 +25,7 @@ class FormViewMixin(RedirectViewMixin, TemplateViewMixin):
         Add the form instance to the template context.
         """
 
-        template_context = TemplateViewMixin._get_template_context(self)
+        template_context = TemplateViewMixin._get_template_context(self) or {}
 
         template_context["form_instance"] = self.form_instance
 
