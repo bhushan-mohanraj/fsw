cc5e165e2bab096c8e903fd37692ebfd9e40d25b
Bhushan Mohanraj <50306448+bhushanmohanraj@users.noreply.github.com>
2022-05-10 13:33:14 -0400

    Add a docstring and organize imports for the `views` package.

--- fsw/views/__init__.py
+++ fsw/views/__init__.py
@@ -1,11 +1,7 @@
-from fsw.views.redirects import (
-    RedirectView,
-)
+"""
+Classes and helpers for creating and improving Flask views.
+"""
 
-from fsw.views.templates import (
-    TemplateView,
-)
-
-from fsw.views.forms import (
-    FormView,
-)
+from fsw.views.redirects import RedirectView
+from fsw.views.templates import TemplateView
+from fsw.views.forms import FormView
