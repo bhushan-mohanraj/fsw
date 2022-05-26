634b710f5706fb7aca69e139fa5069b24dede81d
Bhushan Mohanraj <50306448+bhushanmohanraj@users.noreply.github.com>
2022-05-26 12:42:54 -0400

    Remove the model CRUD mixins.

--- fsw/models/__init__.py
+++ fsw/models/__init__.py
@@ -5,10 +5,3 @@ from fsw.models.id import (
 from fsw.models.name import (
     ClassNameModelMixin,
 )
-
-from fsw.models.crud import (
-    CreateModelMixin,
-    ReadModelMixin,
-    UpdateModelMixin,
-    DeleteModelMixin,
-)
