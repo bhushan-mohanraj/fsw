567e7ae597b45790a844d5bdfc1b0f03f0cd17a9
Bhushan Mohanraj <50306448+bhushanmohanraj@users.noreply.github.com>
2022-06-03 18:19:06 -0400

    Add timestamp mixins for models.

---
+++ fsw/models/timestamp.py
@@ -0,0 +1,68 @@
+"""
+Mixins that add timestamp columns
+that record when a model instance was created, last updated, or deleted.
+"""
+
+import datetime
+
+import sqlalchemy
+
+from fsw.models.base import DatabaseSessionModelMixin
+
+
+class CreateTimestampModelMixin:
+    """
+    A mixin that adds a `created_at` timestamp column,
+    which records when the instance was created (first saved to the database).
+
+    All times are stored in UTC.
+    """
+
+    created_at = sqlalchemy.Column(
+        sqlalchemy.types.DateTime,
+        default=datetime.datetime.utcnow,
+        nullable=False,
+    )
+
+
+class UpdateTimestampModelMixin:
+    """
+    A mixin that adds a `updated_at` timestamp column,
+    which records when the instance was last updated (saved to the database).
+
+    All times are stored in UTC.
+    """
+
+    updated_at = sqlalchemy.Column(
+        sqlalchemy.types.DateTime,
+        default=datetime.datetime.utcnow,
+        onupdate=datetime.datetime.utcnow,
+        nullable=False,
+    )
+
+
+class DeleteTimestampModelMixin(DatabaseSessionModelMixin):
+    """
+    A mixin that adds a `deleted_at` timestamp column and a `delete` method,
+    The mixin emulates deletion by flagging the timestamp with the current time
+    rather than permanently removing the model instance from the database.
+
+    To check that a model instance is not deleted,
+    use `model_instance.deleted_at is None`.
+
+    To query the database for model instances that are not deleted,
+    execute `sqlalchemy.select(Model).where(Model.deleted_at != None)`.
+
+    All times are stored in UTC.
+    """
+
+    deleted_at = sqlalchemy.Column(sqlalchemy.types.DateTime)
+
+    def delete(self) -> None:
+        """
+        Flag the `deleted_at` timestamp column with the current time
+        to indicate that the model instance is deleted.
+        """
+
+        self.deleted_at = datetime.datetime.utcnow()
+        self.database_session.commit()
