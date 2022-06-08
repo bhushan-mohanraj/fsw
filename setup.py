7be7e5ceba6714cf00ff492339c40973537b0475
Bhushan Mohanraj <50306448+bhushanmohanraj@users.noreply.github.com>
2022-06-08 16:39:14 -0400

    Update the repository URL in `setup.py`.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
     description="Tools and guides for building apps with Flask, SQLAlchemy, and WTForms.",
     long_description=readme_path.read_text("utf-8"),
     long_description_content_type="text/markdown",
-    url="https://github.com/bhushanmohanraj/fsw",
+    url="https://github.com/bhushan-mohanraj/fsw",
     author="Bhushan Mohanraj",
     packages=setuptools.find_packages(),
     install_requires=[
