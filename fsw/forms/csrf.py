48f893f490e2f0e7aacee0921dc8ced51033671a
Bhushan Mohanraj <50306448+bhushanmohanraj@users.noreply.github.com>
2022-06-08 16:32:53 -0400

    Enable CSRF protection only when the Flask app context exists.

--- fsw/forms/csrf.py
+++ fsw/forms/csrf.py
@@ -10,11 +10,18 @@ import wtforms.csrf.session
 
 class CSRFProtectFormMixin:
     """
-    Add CSRF protection to forms, using the Flask session.
+    A mixin that adds CSRF protection to forms if the Flask app context exists,
+    using the WTForms implementation of CSRF protection and the Flask session.
     """
 
-    class Meta:
-        csrf = True
-        csrf_class = wtforms.csrf.session.SessionCSRF
-        csrf_secret = secrets.token_hex()
-        csrf_context = flask.session
+    if flask.current_app:
+        class Meta:
+            """
+            A class that enables CSRF protection,
+            using the default WTForms implementation.
+            """
+
+            csrf = True
+            csrf_class = wtforms.csrf.session.SessionCSRF
+            csrf_secret = secrets.token_hex()
+            csrf_context = flask.session
