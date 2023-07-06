960be025d6d196facae91032f7db0f4893627534
Bhushan Mohanraj <50306448+bhushanmohanraj@users.noreply.github.com>
2023-07-06 15:00:17 -0400

    Fix dead references to `fsw.models.name`.

--- fsw/models/__init__.py
+++ fsw/models/__init__.py
@@ -4,8 +4,8 @@ Classes and helpers for SQLAlchemy models.
 
 from fsw.models.delete import HardDeleteModelMixin as HardDeleteModelMixin
 from fsw.models.id import IDModelMixin as IDModelMixin
-from fsw.models.name import ClassNameModelMixin as ClassNameModelMixin
 from fsw.models.save import SaveModelMixin as SaveModelMixin
+from fsw.models.tablename import ClassNameModelMixin as ClassNameModelMixin
 from fsw.models.timestamp import CreateTimestampModelMixin as CreateTimestampModelMixin
 from fsw.models.timestamp import DeleteTimestampModelMixin as DeleteTimestampModelMixin
 from fsw.models.timestamp import UpdateTimestampModelMixin as UpdateTimestampModelMixin
